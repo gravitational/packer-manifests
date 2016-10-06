5cd212f21bbe7643f0cb6908ae5f40265a0382b2
Conor Curlett <conorcurlett@gmail.com>
2016-10-06 10:38:16 -0700

    Updated script to remove explicit version reference in the installer file

--- scripts/ova.sh
+++ scripts/ova.sh
@@ -5,6 +5,6 @@ echo "Adding the Anypoint Installer..."
 
 mkdir -p /opt/anypoint/installer
 cd /opt/anypoint/installer
-cp /tmp/anypoint-1.5.0.tar.gz /opt/anypoint/installer/anypoint-1.5.0.tar.gz
-tar xzvf /opt/anypoint/installer/anypoint-1.5.0.tar.gz
-rm /opt/anypoint/installer/anypoint-1.5.0.tar.gz
+cp /tmp/anypoint-installer.tar.gz /opt/anypoint/installer/anypoint-installer.tar.gz
+tar xzvf /opt/anypoint/installer/anypoint-installer.tar.gz
+rm /opt/anypoint/installer/anypoint-installer.tar.gz
